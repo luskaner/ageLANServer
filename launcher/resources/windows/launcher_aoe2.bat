64f94a79f772d5b6ca8e2a23cd037b3e87f4ec61
David Fernández Aldana <luskaner@gmail.com>
2024-11-03 23:50:59 +0100

    Fixed launcher scripts wrong game config argument

--- launcher/resources/windows/launcher_aoe2.bat
+++ launcher/resources/windows/launcher_aoe2.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-launcher -e age2 -c resources/config.aoe2.toml
+launcher -e age2 --gameConfig resources/config.aoe2.toml
 pause
--- launcher/resources/windows/launcher_aoe3.bat
+++ launcher/resources/windows/launcher_aoe3.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-launcher -e age3 -c resources/config.aoe3.toml
+launcher -e age3 --gameConfig resources/config.aoe3.toml
 pause
