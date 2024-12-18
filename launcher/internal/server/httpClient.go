0d16640e2072ed7420f9ae414fd991303fe4c206
David Fernández Aldana <luskaner@gmail.com>
2024-12-18 18:47:41 +0100

    Enhancement: make sure we use the built-in curl binary on windows (launcher)

--- launcher/internal/server/httpClient.go
+++ launcher/internal/server/httpClient.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"github.com/luskaner/ageLANServer/launcher-common/executor/exec"
+	"os"
+	"path/filepath"
+	"runtime"
 )
 
 func HttpGet(url string, insecureSkipVerify bool) int {
@@ -10,8 +13,14 @@ func HttpGet(url string, insecureSkipVerify bool) int {
 		args = append(args, "-k")
 	}
 	args = append(args, url)
+	var file string
+	if runtime.GOOS == "windows" {
+		// Make sure we use the built-in curl on Windows so it uses the system certificate store
+		file = filepath.Join(os.Getenv("WINDIR"), "System32")
+	}
+	file = filepath.Join(file, "curl")
 	result := exec.Options{
-		File:        "curl",
+		File:        file,
 		Args:        args,
 		SpecialFile: true,
 		Wait:        true,
