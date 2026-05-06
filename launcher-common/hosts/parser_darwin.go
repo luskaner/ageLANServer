1be62083b1a7c8e7533d4af0c522169402f3d81e
David Fernández Aldana <luskaner@gmail.com>
2026-05-06 02:06:20 +0200

    improved previous commit

--- launcher-common/hosts/parser_darwin.go
+++ launcher-common/hosts/parser_darwin.go
@@ -1,14 +1,22 @@
 package hosts
 
 import (
+	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
+const markerStr = string(commentMarker)
+
 func splitLine(line string) []string {
+	if strings.HasPrefix(line, markerStr) {
+		return []string{"", line[len(markerStr):]}
+	}
 	var prevWasSpace bool
 	for i, r := range line {
 		if r == commentMarker && prevWasSpace {
-			return []string{line[:i], line[i+1:]}
+			cut := i + utf8.RuneLen(r)
+			return []string{line[:i], line[cut:]}
 		}
 		prevWasSpace = unicode.IsSpace(r)
 	}
