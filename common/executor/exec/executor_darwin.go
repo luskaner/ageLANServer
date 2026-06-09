692b435169c0ca58d41828ae8e1e3a4df009090b
David Fernández Aldana <luskaner@gmail.com>
2026-06-09 19:51:06 +0200

    Further macos fixes

--- common/executor/exec/executor_darwin.go
+++ common/executor/exec/executor_darwin.go
@@ -4,11 +4,28 @@ func terminalArgs() []arg {
 	script := `
 		on run argv
 			set AppleScript's text item delimiters to " "
-			set cmd to argv as string
+			set cmd to argv as string	
+			set tmpFile to do shell script "mktemp /tmp/ageLANServer_osascript_exit_code.XXXXXX"
+			set fullCmd to cmd & "; echo $? > " & tmpFile
+		
 			tell application "Terminal"
-				activate
-				do script cmd
+				set w to (do script fullCmd)
+				set winID to id of w
 			end tell
+		
+			tell application "System Events" to tell process "Terminal"
+				set miniaturized of (first window whose id is winID) to true
+			end tell
+		
+			tell application "Terminal"
+				repeat while busy of w is true
+					delay 0.1
+				end repeat
+				close w
+			end tell
+		
+			set exitCode to do shell script "cat " & tmpFile & "; rm " & tmpFile
+			return exitCode
 		end run
 	`
 	return osascriptArgs(script)
