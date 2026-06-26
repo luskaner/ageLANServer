6a409cc4038a82fbac59db033f9398dbc25a66be
David Fernández Aldana <luskaner@gmail.com>
2026-06-26 21:09:11 +0200

    more fixes

--- common/process/process_darwin.go
+++ common/process/process_darwin.go
@@ -2,6 +2,7 @@ package process
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"os"
 	"strings"
@@ -12,34 +13,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-const (
-	procAllPids = 1
-)
-
-type ProcPidBsdInfo struct {
-	PbiFlags     uint32
-	PbiStatus    uint32
-	PbiXstatus   uint32
-	PbiPid       uint32
-	PbiPpid      uint32
-	PbiUid       uint32
-	PbiGid       uint32
-	PbiRuid      uint32
-	PbiRgid      uint32
-	PbiSvuid     uint32
-	PbiSvgid     uint32
-	Rfu1         uint32
-	PbiComm      [16]byte
-	PbiName      [1024]byte
-	PbiNfiles    uint32
-	PbiPgid      uint32
-	PbiPjobc     uint32
-	PbiTdev      uint32
-	PbiTpgid     uint32
-	PbiNice      uint32
-	PbiStartSec  uint64
-	PbiStartUsec uint64
-}
+const procAllPids = 1
 
 var (
 	libHandle       uintptr
@@ -48,6 +22,10 @@ var (
 	procPidinfoPtr  func(int32, int32, uint64, uintptr, int32) int32
 	procListpidsPtr func(uint32, uint32, uintptr, int32) int32
 	procPidBsdInfo  int32
+	procBsdInfoSize int32
+	offsetPid       int
+	offsetStartSec  int
+	offsetStartUsec int
 )
 
 func loadLib() {
@@ -61,21 +39,42 @@ func loadLib() {
 		purego.RegisterLibFunc(&procPidinfoPtr, libHandle, "proc_pidinfo")
 		purego.RegisterLibFunc(&procListpidsPtr, libHandle, "proc_listpids")
 
-		var info ProcPidBsdInfo
-		size := int32(unsafe.Sizeof(info))
-		buf := uintptr(unsafe.Pointer(&info))
 		pid := int32(os.Getpid())
+		buf := make([]byte, 4096)
 
 		for f := int32(0); f < 256; f++ {
-			r := procPidinfoPtr(pid, f, 0, buf, size)
-			if r == size && info.PbiPid == uint32(pid) {
-				procPidBsdInfo = f
+			r := procPidinfoPtr(pid, f, 0, uintptr(unsafe.Pointer(&buf[0])), int32(len(buf)))
+			if r > 0 {
+				for off := 0; off+4 <= int(r); off += 4 {
+					if binary.LittleEndian.Uint32(buf[off:off+4]) == uint32(pid) {
+						procPidBsdInfo = f
+						procBsdInfoSize = r
+						offsetPid = off
+						break
+					}
+				}
+			}
+			if procPidBsdInfo != 0 {
 				break
 			}
 		}
 
 		if procPidBsdInfo == 0 {
 			loadErr = fmt.Errorf("could not detect PROC_PIDBSDINFO flavor")
+			return
+		}
+
+		for off := offsetPid + 4; off+8 <= int(procBsdInfoSize); off += 8 {
+			sec := binary.LittleEndian.Uint64(buf[off : off+8])
+			if sec > 1000000000 && sec < 5000000000 {
+				offsetStartSec = off
+				offsetStartUsec = off + 8
+				break
+			}
+		}
+
+		if offsetStartSec == 0 {
+			loadErr = fmt.Errorf("could not detect start time offsets")
 		}
 	})
 }
@@ -85,13 +84,17 @@ func GetProcessStartTime(pid int) (int64, error) {
 	if loadErr != nil {
 		return 0, loadErr
 	}
-	var info ProcPidBsdInfo
-	size := int32(unsafe.Sizeof(info))
-	ret := procPidinfoPtr(int32(pid), procPidBsdInfo, 0, uintptr(unsafe.Pointer(&info)), size)
-	if ret != size {
+
+	buf := make([]byte, procBsdInfoSize)
+	r := procPidinfoPtr(int32(pid), procPidBsdInfo, 0, uintptr(unsafe.Pointer(&buf[0])), procBsdInfoSize)
+	if r != procBsdInfoSize {
 		return 0, unix.EPERM
 	}
-	return int64(info.PbiStartSec*1e6 + info.PbiStartUsec), nil
+
+	sec := binary.LittleEndian.Uint64(buf[offsetStartSec : offsetStartSec+8])
+	usec := binary.LittleEndian.Uint64(buf[offsetStartUsec : offsetStartUsec+8])
+
+	return int64(sec*1e6 + usec), nil
 }
 
 func ProcessesByNames(names []string) map[string]*os.Process {
@@ -100,17 +103,19 @@ func ProcessesByNames(names []string) map[string]*os.Process {
 		return result
 	}
 	loadLib()
-	if loadErr != nil || procListpidsPtr == nil || procPidinfoPtr == nil {
+	if loadErr != nil {
 		for _, n := range names {
 			result[n] = nil
 		}
 		return result
 	}
+
 	targets := make([]string, len(names))
 	for i, n := range names {
 		targets[i] = strings.ToLower(n)
 		result[n] = nil
 	}
+
 	const maxPids = 16384
 	pidBuf := make([]int32, maxPids)
 	bufBytes := int32(len(pidBuf) * int(unsafe.Sizeof(pidBuf[0])))
@@ -118,31 +123,36 @@ func ProcessesByNames(names []string) map[string]*os.Process {
 	if ret <= 0 {
 		return result
 	}
+
 	numPids := int(ret) / int(unsafe.Sizeof(pidBuf[0]))
 	if numPids > len(pidBuf) {
 		numPids = len(pidBuf)
 	}
+
 	remaining := make([]bool, len(names))
 	for i := range remaining {
 		remaining[i] = true
 	}
 	remainingCount := len(names)
+
 	for i := 0; i < numPids && remainingCount > 0; i++ {
 		pid := pidBuf[i]
 		if pid <= 0 {
 			continue
 		}
-		var info ProcPidBsdInfo
-		size := int32(unsafe.Sizeof(info))
-		r := procPidinfoPtr(pid, procPidBsdInfo, 0, uintptr(unsafe.Pointer(&info)), size)
-		if r != size {
+
+		buf := make([]byte, procBsdInfoSize)
+		r := procPidinfoPtr(pid, procPidBsdInfo, 0, uintptr(unsafe.Pointer(&buf[0])), procBsdInfoSize)
+		if r != procBsdInfoSize {
 			continue
 		}
-		nameBytes := info.PbiName[:]
+
+		nameBytes := buf[offsetPid+4 : offsetPid+4+1024]
 		if idx := bytes.IndexByte(nameBytes, 0); idx >= 0 {
 			nameBytes = nameBytes[:idx]
 		}
 		name := strings.ToLower(string(nameBytes))
+
 		for ti, t := range targets {
 			if !remaining[ti] {
 				continue
@@ -156,5 +166,6 @@ func ProcessesByNames(names []string) map[string]*os.Process {
 			}
 		}
 	}
+
 	return result
 }
