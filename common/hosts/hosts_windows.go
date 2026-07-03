b75bfcc98ee5eb77c08c0f7122d64e7a335e502f
David Fernández Aldana <luskaner@gmail.com>
2026-07-03 23:35:06 +0200

    MacOS and Linux: when adding hosts comment out mappings to the hostname

--- common/hosts/hosts_windows.go
+++ common/hosts/hosts_windows.go
@@ -10,3 +10,8 @@ const LineEnding = WindowsLineEnding
 func Path() string {
 	return filepath.Join(os.Getenv("WINDIR"), "System32", "drivers", "etc", "hosts")
 }
+
+func commentHost(_ Host) bool {
+	// Windows ignores any mapping to its hostname
+	return false
+}
