2293d91cd1ecfe6456cf95162539e0f5193c09a7
David Fernández Aldana <luskaner@users.noreply.github.com>
2026-07-03 23:41:32 +0200

    MacOS and Linux: when adding hosts comment out mappings to the hostname (#550)

--- common/hosts/hosts_unix.go
+++ common/hosts/hosts_unix.go
@@ -2,8 +2,23 @@
 
 package hosts
 
+import (
+	"os"
+)
+
 const LineEnding = "\n"
 
 func Path() string {
 	return "/etc/hosts"
+}
+
+var hostname Host
+
+func commentHost(host Host) bool {
+	if hostname == "" {
+		if name, err := os.Hostname(); err == nil {
+			_, hostname = parseHost(name)
+		}
+	}
+	return host == hostname
 }
