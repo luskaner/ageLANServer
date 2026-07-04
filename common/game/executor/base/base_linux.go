7a0c1b81b6bf601ab80bd3fa262ee56def251b1b
David Fernández Aldana <luskaner@gmail.com>
2026-07-04 23:11:15 +0200

    Linux: fix not knowing if it's successful

--- common/game/executor/base/base_linux.go
+++ common/game/executor/base/base_linux.go
@@ -3,7 +3,7 @@ package base
 import commonExecutor "github.com/luskaner/ageLANServer/common/executor/exec"
 
 func StartUri(uri string, optionsFn func(options commonExecutor.Options)) (result *commonExecutor.Result) {
-	options := commonExecutor.Options{File: "xdg-open", Args: []string{uri}, SpecialFile: true, Pid: true}
+	options := commonExecutor.Options{File: "xdg-open", Args: []string{uri}, SpecialFile: true, ExitCode: true, Wait: true}
 	optionsFn(options)
 	result = options.Exec()
 	return
