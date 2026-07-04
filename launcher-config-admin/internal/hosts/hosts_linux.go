5a92426af43d5fbbce5870c4af075074f3b48e52
David Fernández Aldana <luskaner@users.noreply.github.com>
2026-07-04 21:25:34 +0200

    543 age of empire iv failed to read certificate from aoe apireliclinkcom (#551)
    * Linux: restart service instead of resolved if available

--- launcher-config-admin/internal/hosts/hosts_linux.go
+++ launcher-config-admin/internal/hosts/hosts_linux.go
@@ -1,7 +1,43 @@
 package hosts
 
-import "github.com/luskaner/ageLANServer/common/executor/exec"
+import (
+	"io"
+
+	"github.com/hairyhenderson/go-which"
+	"github.com/luskaner/ageLANServer/common"
+	"github.com/luskaner/ageLANServer/common/executor/exec"
+	commonLogger "github.com/luskaner/ageLANServer/common/logger"
+	"github.com/luskaner/ageLANServer/launcher-config-admin/internal"
+)
 
 func FlushDns() (result *exec.Result) {
+	path := which.Which("resolvectl")
+	if path != "" {
+		commonLogger.Println("Flushing DNS cache...")
+		options := exec.Options{File: "systemctl", SpecialFile: true, UseWorkingPath: true, ExitCode: true, Wait: true, Args: []string{"restart", "systemd-resolved"}}
+		var suffix string
+		if internal.SetUp == nil {
+			suffix = "_flushCache"
+		} else if *internal.SetUp {
+			suffix = "_setup"
+		} else {
+			suffix = "_revert"
+		}
+		if err := internal.Logger.Buffer("resolvectl_flushcache"+suffix, func(writer io.Writer) {
+			if writer != nil {
+				options.Stdout = writer
+				options.Stderr = writer
+				commonLogger.Printf("run resolvectl: %s\n", options.String())
+			}
+			result = options.Exec()
+		}); err != nil {
+			result = &exec.Result{
+				ExitCode: common.ErrFileLog,
+				Err:      err,
+			}
+		}
+		return
+	}
+	// Some systems do not have, assume it is not needed
 	return exec.ResultSuccess
 }
